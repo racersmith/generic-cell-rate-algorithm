688eaa91b4cd9666dcb3ae11048d791a7aade78d
Josh Smith <racersmith@gmail.com>
2025-03-27 07:55:03 -0600

    adding comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,8 +12,6 @@ from generic_cell_rate_algorithm import throttle
 from generic_cell_rate_algorithm.throttle import RateLimit
 
 
-
-
 def _at_least_1d(x: Any) -> List[Any]:
     """ Ensure that x is a list """
     if not isinstance(x, (list, set, tuple)):
@@ -25,13 +23,16 @@ def _at_least_1d(x: Any) -> List[Any]:
 
 
 class TimeWarp(throttle.Time):
+    """ Time without all the waiting. """
     def __init__(self):
         self.clock = 0.0
 
     def time(self) -> float:
+        """ return the current clock time """
         return self.clock
 
     def sleep(self, seconds: float) -> None:
+        """ Simulate sleeping by moving the clock forward """
         if seconds < 0:
             raise ValueError("sleep length must be non-negative")
         self.clock += seconds
@@ -43,6 +44,7 @@ def time():
 
 
 class MockEndpoint:
+    """ Create a mock endpoint that enforces one or more rate limit policies """
     def __init__(self, time: TimeWarp, rate_limit: RateLimit | List[RateLimit], fixed_period=False):
         self._requests = list()
         self.time = time
@@ -59,6 +61,9 @@ class MockEndpoint:
 
     @staticmethod
     def _update_usage(now: float, last_timestamp: float, rate_limit: RateLimit):
+        """ Update the rate limit usage and reset the usage when new periods are entered.
+        This simulates that if an endpoint uses fixed periods for their policy that rest at the start of each period.
+        """
         if now // rate_limit.period > last_timestamp // rate_limit.period:
             # The current time is in a different period than the last timestamp.
             rate_limit.usage = 0
@@ -66,6 +71,7 @@ class MockEndpoint:
         rate_limit.usage += 1
 
     def verify_fixed_period(self, now, rate_limits):
+        """ Verify that if the rate limit usage is reset at fixed intervals that we don't exceed the allowed count """
         last_timestamp = list([self._requests[-1]] + self._requests)[-2]
         for rate_limit in rate_limits:
             self._update_usage(now, last_timestamp, rate_limit)
@@ -77,6 +83,7 @@ class MockEndpoint:
                 )
 
     def verify_rolling_period(self, rate_limits):
+        """ Verify that in the last period we have not exceeded the count for each rate limit """
         for rate_limit in rate_limits:
             start_of_period = self._requests[-1] - rate_limit.period
             n_in_period = np.sum(np.array(self._requests) > start_of_period)
@@ -87,6 +94,7 @@ class MockEndpoint:
 
 
 class PriorityFunction:
+    """ Wrapper for adding throttle arguments to a function """
     def __init__(self, gcra: throttle.GCRA, fn: Callable):
         self.gcra = gcra
         self.fn = fn
@@ -102,10 +110,12 @@ throttle_state_row = namedtuple("throttle_state_row", ["id", "level", "tat", "al
 
 
 class ThrottleStateInterface(throttle.ThrottleStateIO):
+    """ Create a interface to a mock DB for throttle states """
     def __init__(self, throttle_state_list: List[throttle_state_row]):
         self._db = {state.id: state for state in throttle_state_list}
 
     def read(self) -> List[throttle.ThrottleState]:
+        """ Read from our mock DB and provide ThrottleStates"""
         result = list()
         for row in self._db.values():
             throttle_state = throttle.ThrottleState(row.tat, row.level, row.allocation, id=row.id)
@@ -114,20 +124,24 @@ class ThrottleStateInterface(throttle.ThrottleStateIO):
         return result
 
     def write(self, previous: throttle.ThrottleState, new: throttle.ThrottleState):
+        """ Write the new throttle state into the DB """
         self._db[previous.id] = throttle_state_row(**new.__dict__)
 
-    # def transaction_context(self) -> AbstractContextManager:
-    #     return Lock()
+    def transaction_context(self) -> AbstractContextManager:
+        """ Provide a transaction context for this throttle state """
+        return Lock()
 
 
 @pytest.fixture
 def throttle_state_io():
+    """ Mock and interface to our throttle state DB with single state """
     states = [throttle_state_row(id=123, level=0, tat=0, allocation=1)]
     return ThrottleStateInterface(states)
 
 
 @pytest.fixture
 def priority_throttle_state_io():
+    """ Mock and interface to our throttle state DB with priority levels """
     states = [
         throttle_state_row(id=234, level=0, tat=0, allocation=2),
         throttle_state_row(id=345, level=1, tat=0, allocation=1),
@@ -136,6 +150,7 @@ def priority_throttle_state_io():
 
 
 class RateLimitInterface(throttle.RateLimitIO):
+    """ Mock an interface to a DB that holds information on our Rate Limits """
     def __init__(self, rate_limits: throttle.RateLimit | List[throttle.RateLimit]):
         self.rate_limits = rate_limits
 
@@ -145,12 +160,14 @@ class RateLimitInterface(throttle.RateLimitIO):
 
 @pytest.fixture
 def single_rate_limit_io():
+    """ Provide a rate limit interface with a single rate limit """
     rate_limits = throttle.RateLimit(count=60, period=900)
     return RateLimitInterface(rate_limits)
 
 
 @pytest.fixture
 def multi_rate_limit_io():
+    """ Provide a rate limit interface with two rate limits. """
     rate_limits = [
         throttle.RateLimit(count=60, period=900),
         throttle.RateLimit(count=600, period=86400),
@@ -159,6 +176,7 @@ def multi_rate_limit_io():
 
 
 class System:
+    """ Package a test system together that has everything setup and ready to run """
     def __init__(self, rate_limit_io, throttle_state_io, fixed_period: bool):
         self.time = TimeWarp()
         self.gcra = throttle.GCRA(
@@ -180,19 +198,3 @@ class System:
             return self.endpoint()
 
         return _fn()
-
-
-
-# @pytest.fixture
-# def gcra_simple(single_rate_limit_io, throttle_io, time):
-#     return throttle.GCRA(single_rate_limit_io, throttle_io, time)
-#
-#
-# @pytest.fixture
-# def gcra_priority(single_rate_limit_io, priority_throttle_state_io, time):
-#     return throttle.GCRA(single_rate_limit_io, priority_throttle_state_io, time)
-#
-#
-# def setup(gcra: throttle.GCRA):
-#     endpoint = MockEndpoint(gcra, fixed_period=False)
-#     fn = PriorityFunction(gcra, endpoint)
